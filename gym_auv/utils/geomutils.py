e7bd0e2a0198a28ee9fadc55113f207d6f5aed69
Simen Theie Havenstrøm <simentha@ntnu.no>
2019-09-26 11:59:01 +0200

    Added J(eta) to geomutil

--- gym_auv/utils/geomutils.py
+++ gym_auv/utils/geomutils.py
@@ -4,6 +4,7 @@ import numpy as np
 def princip(angle):
     return ((angle + np.pi) % (2*np.pi)) - np.pi
 
+
 def Rzyx(phi, theta, psi):
     cphi = np.cos(phi)
     sphi = np.sin(phi)
@@ -17,3 +18,28 @@ def Rzyx(phi, theta, psi):
         np.hstack([spsi*cth, cpsi*cphi+sphi*sth*spsi, -cpsi*sphi+sth*spsi*cphi]),
         np.hstack([-sth, cth*sphi, cth*cphi])
     ])
+
+
+def Tzyx(phi, theta, psi):
+    sphi = np.sin(phi)
+    tth = np.tan(theta)
+    cphi = np.cos(phi)
+    cth = np.cos(theta)
+
+    return np.vstack([[1, sphi*tth, cphi*tth], 
+                      [0, cphi, -sphi],
+                      [0, sphi/cth, cphi/cth]])
+
+
+def J(eta):
+    phi = eta[3]
+    theta = eta[4]
+    psi = eta[5]
+
+    R = Rzyx(phi, theta, psi)
+    T = Tzyx(phi, theta, psi)
+    zero = np.zeros((3,3))
+
+    J = np.vstack([np.hstack([R, zero]),
+                   np.hstack([zero, T])])
+    return J
